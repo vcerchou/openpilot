6eb421021632ec6dc6391086e0114b007493e692
“winnie” <vcer@foxmail.com>
2023-04-27 21:29:37 +0800

    支持Ti
    更新Ti
    
    fix Ti
    
    fix Ti
    
    Update log.capnp
    
    Update __init__.py
    
    Update safety_mazda.h

--- panda/python/__init__.py
+++ panda/python/__init__.py
@@ -200,8 +200,9 @@ class Panda:
 
   CAN_PACKET_VERSION = 4
   HEALTH_PACKET_VERSION = 14
+  
   CAN_HEALTH_PACKET_VERSION = 5
-  HEALTH_STRUCT = struct.Struct("<IIIIIIIIIBBBBBBHBBBHfBBHBHH")
+  HEALTH_STRUCT = struct.Struct("<IIIIIIIIIBBBBBBHBBBHfBBHBHHB")
   CAN_HEALTH_STRUCT = struct.Struct("<BIBBBBBBBBIIIIIIIHHBBBIIII")
 
   F2_DEVICES = (HW_TYPE_PEDAL, )
@@ -631,6 +632,7 @@ class Panda:
       "fan_stall_count": a[24],
       "sbu1_voltage_mV": a[25],
       "sbu2_voltage_mV": a[26],
+      "torque_interceptor_detected": a[27],
     }
 
   @ensure_can_health_packet_version
