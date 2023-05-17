e1c1b5cd7e8b3a0af4fe54c77f55e7386df060c8
vcerchou <31274535+vcerchou@users.noreply.github.com>
2023-05-17 08:43:06 +0800

    纵向调优
    更新 interface.py
    
    纵向调优2

--- selfdrive/car/mazda/interface.py
+++ selfdrive/car/mazda/interface.py
@@ -21,13 +21,13 @@ class CarInterface(CarInterfaceBase):
 
     ret.openpilotLongitudinalControl = True
     ret.longitudinalTuning.kpBP = [0., 5., 30.]
-    ret.longitudinalTuning.kpV = [1.3, 1.0, 0.7]
-    ret.longitudinalTuning.kiBP = [0., 5., 20., 30.]
-    ret.longitudinalTuning.kiV = [0.36, 0.23, 0.17, 0.1]
-    ret.longitudinalTuning.deadzoneBP = [0.0, 30.0]
-    ret.longitudinalTuning.deadzoneV = [0.0, 0.03]
-    ret.longitudinalActuatorDelayLowerBound = 0.3
-    ret.longitudinalActuatorDelayUpperBound = 1.5
+    ret.longitudinalTuning.kpV = [1.2, 0.8, 0.5]
+    ret.longitudinalTuning.kiBP = [0., 35.]
+    ret.longitudinalTuning.kiV = [0.18, 0.12]
+    #ret.longitudinalTuning.deadzoneBP = [0.0, 30.0]
+    #ret.longitudinalTuning.deadzoneV = [0.0, 0.03]
+    #ret.longitudinalActuatorDelayLowerBound = 0.5
+    #ret.longitudinalActuatorDelayUpperBound = 0.5
     
     ret.steerActuatorDelay = 0.1
     ret.steerLimitTimer = 0.8
