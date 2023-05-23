05e061d40a6c8024c2f95d42c8808d8708e81039
vcerchou <31274535+vcerchou@users.noreply.github.com>
2023-05-23 10:52:34 +0800

    距离控制测试1

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from cereal import car
 from common.realtime import sec_since_boot
-from common.numpy_fast import clip
+from common.numpy_fast import clip, interp
 from system.swaglog import cloudlog
 # WARNING: imports outside of constants will not trigger a rebuild
 from selfdrive.modeld.constants import index_function
@@ -59,29 +59,33 @@ MIN_ACCEL = -3.5
 MAX_ACCEL = 2.0
 COMFORT_BRAKE = 2.5
 STOP_DISTANCE = 6.0
+T_FOLLOW_CHILL = 2.44
+T_FOLLOW_NORMAL = 1.96
+T_FOLLOW_AGGRO = 1.45
+T_FOLLOW_DANGER = 1.1
 
-def get_T_FOLLOW(set_distance=SetDistance.normal):
+
+def get_desired_tf(set_distance=SetDistance.normal):
   if set_distance == SetDistance.aggresive:
-    return 1.1
+    return T_FOLLOW_AGGRO
   elif set_distance == SetDistance.normal:
-    return 1.45
+    return T_FOLLOW_NORMAL
   elif set_distance == SetDistance.chill:
-    return 1.96
-  elif set_distance == SetDistance.auto:
-    return 2.44
+    return T_FOLLOW_CHILL
+  elif set_distance == SetDistance.experimental:
+    return T_FOLLOW_DANGER
   else:
-    return 1.45
-
-T_FOLLOW = get_T_FOLLOW()
+    return T_FOLLOW_NORMAL
+    
 
 def get_stopped_equivalence_factor(v_lead):
   return (v_lead**2) / (2 * COMFORT_BRAKE)
 
-def get_safe_obstacle_distance(v_ego, t_follow=T_FOLLOW):
-  return (v_ego**2) / (2 * COMFORT_BRAKE) + t_follow * v_ego + STOP_DISTANCE
+def get_safe_obstacle_distance(v_ego, desired_tf):
+  return (v_ego**2) / (2 * COMFORT_BRAKE) + desired_tf * v_ego + STOP_DISTANCE - 1.25 + desired_tf
 
-def desired_follow_distance(v_ego, v_lead, t_follow=T_FOLLOW):
-  return get_safe_obstacle_distance(v_ego, t_follow) - get_stopped_equivalence_factor(v_lead)
+def desired_follow_distance(v_ego, v_lead, desired_tf = get_desired_tf()):
+  return get_safe_obstacle_distance(v_ego, desired_tf) - get_stopped_equivalence_factor(v_lead)
 
 
 def gen_long_model():
@@ -177,7 +181,7 @@ def gen_long_ocp():
 
   x0 = np.zeros(X_DIM)
   ocp.constraints.x0 = x0
-  ocp.parameter_values = np.array([-1.2, 1.2, 0.0, 0.0, T_FOLLOW, LEAD_DANGER_FACTOR])
+  ocp.parameter_values = np.array([-1.2, 1.2, 0.0, 0.0, T_FOLLOW_CHILL, LEAD_DANGER_FACTOR])
 
   # We put all constraint cost weights to 0 and only set them at runtime
   cost_weights = np.zeros(CONSTR_DIM)
@@ -219,6 +223,7 @@ class LongitudinalMpc:
     self.solver = AcadosOcpSolverCython(MODEL_NAME, ACADOS_SOLVER_TYPE, N)
     self.reset()
     self.source = SOURCES[2]
+    self.desired_tf = T_FOLLOW_NORMAL
 
   def reset(self):
     # self.solver = AcadosOcpSolverCython(MODEL_NAME, ACADOS_SOLVER_TYPE, N)
@@ -249,12 +254,22 @@ class LongitudinalMpc:
     self.x0 = np.zeros(X_DIM)
     self.set_weights()
 
+  # make sure the multipliers are simulated for reliability
+  # test them using test/test_longitudinal.py
+  def get_cost_multipliers(self):
+    tfs = [T_FOLLOW_AGGRO, T_FOLLOW_NORMAL, T_FOLLOW_CHILL]
+    a_change_tf = interp(self.desired_tf, tfs, [0.9, 1., 1.1])
+    j_ego_tf = interp(self.desired_tf, tfs, [1., 1., 1.])
+    d_zone_tf = interp(self.desired_tf, tfs, [1., 0.9, 0.8])
+    return (a_change_tf, j_ego_tf, d_zone_tf)
+
   def set_cost_weights(self, cost_weights, constraint_cost_weights):
+    cost_mult = self.get_cost_multipliers()
     W = np.asfortranarray(np.diag(cost_weights))
     for i in range(N):
       # TODO don't hardcode A_CHANGE_COST idx
       # reduce the cost on (a-a_prev) later in the horizon.
-      W[4,4] = cost_weights[4] * np.interp(T_IDXS[i], [0.0, 1.0, 2.0], [1.0, 1.0, 0.0])
+      W[4,4] = cost_weights[4]* cost_mult[0] * np.interp(T_IDXS[i], [0.0, 1.0, 2.0], [1.0, 1.0, 0.0])
       self.solver.cost_set(i, 'W', W)
     # Setting the slice without the copy make the array not contiguous,
     # causing issues with the C interface.
@@ -324,7 +339,7 @@ class LongitudinalMpc:
     self.max_a = max_a
 
   def update(self, radarstate, v_cruise, x, v, a, j):
-    T_FOLLOW = get_T_FOLLOW(SetDistance)
+    self.desired_tf = get_desired_tf(car.carstate.cruiseState.setDistance)
     v_ego = self.x0[1]
     self.status = radarstate.leadOne.status or radarstate.leadTwo.status
 
@@ -351,7 +366,7 @@ class LongitudinalMpc:
       v_cruise_clipped = np.clip(v_cruise * np.ones(N+1),
                                  v_lower,
                                  v_upper)
-      cruise_obstacle = np.cumsum(T_DIFFS * v_cruise_clipped) + get_safe_obstacle_distance(v_cruise_clipped, T_FOLLOW)
+      cruise_obstacle = np.cumsum(T_DIFFS * v_cruise_clipped) + get_safe_obstacle_distance(v_cruise_clipped, self.desired_tf)
       x_obstacles = np.column_stack([lead_0_obstacle, lead_1_obstacle, cruise_obstacle])
       self.source = SOURCES[np.argmin(x_obstacles[0])]
 
@@ -385,7 +400,7 @@ class LongitudinalMpc:
 
     self.params[:,2] = np.min(x_obstacles, axis=1)
     self.params[:,3] = np.copy(self.prev_a)
-    self.params[:,4] = T_FOLLOW
+    self.params[:,4] = self.desired_tf
 
     self.run()
     if (np.any(lead_xv_0[FCW_IDXS,0] - self.x_sol[FCW_IDXS,0] < CRASH_DISTANCE) and
@@ -397,9 +412,9 @@ class LongitudinalMpc:
     # Check if it got within lead comfort range
     # TODO This should be done cleaner
     if self.mode == 'blended':
-      if any((lead_0_obstacle - get_safe_obstacle_distance(self.x_sol[:,1], T_FOLLOW))- self.x_sol[:,0] < 0.0):
+      if any((lead_0_obstacle - get_safe_obstacle_distance(self.x_sol[:,1], self.desired_tf))- self.x_sol[:,0] < 0.0):
         self.source = 'lead0'
-      if any((lead_1_obstacle - get_safe_obstacle_distance(self.x_sol[:,1], T_FOLLOW))- self.x_sol[:,0] < 0.0) and \
+      if any((lead_1_obstacle - get_safe_obstacle_distance(self.x_sol[:,1], self.desired_tf))- self.x_sol[:,0] < 0.0) and \
          (lead_1_obstacle[0] - lead_0_obstacle[0]):
         self.source = 'lead1'
 
