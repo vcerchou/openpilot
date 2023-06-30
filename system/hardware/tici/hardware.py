fa6e6b82bd58ee761850fd9f7ea649482f0e0c62
vcerchou <31274535+vcerchou@users.noreply.github.com>
2023-06-30 16:23:31 +0800

    在侧边栏显示IP
    Update log.capnp
    
    Update hardware.py
    
    Update hardware.py
    
    Revert "Update thermald.py"
    
    This reverts commit 1b3ae347164c92f86954a669744013c2919858dd.
    
    Update thermald.py
    
    修复sidebar wifi显示

--- system/hardware/tici/hardware.py
+++ system/hardware/tici/hardware.py
@@ -1,6 +1,7 @@
 import json
 import math
 import os
+import re
 import subprocess
 import time
 from enum import IntEnum
@@ -584,7 +585,14 @@ class Tici(HardwareBase):
     time.sleep(1)
     gpio_set(GPIO.STM_BOOT0, 0)
 
-
+  def get_ip_address(self):
+    try:
+      wlan = subprocess.check_output(["ifconfig", "wlan0"], encoding='utf8').strip()
+      pattern = re.compile(r'inet (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
+      return pattern.search(wlan).group(1)
+    except Exception:
+      return "────────"
+    
 if __name__ == "__main__":
   t = Tici()
   t.initialize_hardware()
