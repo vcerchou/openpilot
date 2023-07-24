16ac90cee61cabb5f7cf3fc6d8500adcc8321de8
“winnie” <vcer@foxmail.com>
2023-07-24 19:35:57 +0800

    Revert "Update values.py"
    This reverts commit faad2bf36edaa9408d29c6e5f454f1003ad87bc7.

--- selfdrive/car/mazda/values.py
+++ selfdrive/car/mazda/values.py
@@ -12,10 +12,10 @@ Ecu = car.CarParams.Ecu
 # Steer torque limits
 
 class CarControllerParams:
-  STEER_MAX = 800                # theoretical max_steer 2047
+  STEER_MAX = 600                # theoretical max_steer 2047
   STEER_DELTA_UP = 10             # torque increase per refresh
   STEER_DELTA_DOWN = 25           # torque decrease per refresh
-  STEER_DRIVER_ALLOWANCE = 15     # allowed driver torque before start limiting
+  STEER_DRIVER_ALLOWANCE = 5     # allowed driver torque before start limiting
   STEER_DRIVER_MULTIPLIER = 40     # weight driver torque
   STEER_DRIVER_FACTOR = 1         # from dbc
   STEER_ERROR_MAX = 350           # max delta between torque cmd and torque motor
@@ -24,7 +24,7 @@ class CarControllerParams:
   TI_STEER_MAX = 600                # theoretical max_steer 2047
   TI_STEER_DELTA_UP = 6             # torque increase per refresh
   TI_STEER_DELTA_DOWN = 15           # torque decrease per refresh
-  TI_STEER_DRIVER_ALLOWANCE = 15    # allowed driver torque before start limiting
+  TI_STEER_DRIVER_ALLOWANCE = 5    # allowed driver torque before start limiting
   TI_STEER_DRIVER_MULTIPLIER = 40     # weight driver torque
   TI_STEER_DRIVER_FACTOR = 1         # from dbc
   TI_STEER_ERROR_MAX = 350           # max delta between torque cmd and torque motor
